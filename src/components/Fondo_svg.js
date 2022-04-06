323c3fb9f4187b2f5729d56667647c4f194f187f
alejo16 <alejandrovalencia2011@gmail.com>
2022-04-06 15:41:18 -0600

    alinear

--- src/components/Fondo_svg.js
+++ src/components/Fondo_svg.js
@@ -4,15 +4,15 @@ import PlayerPersonPerson from '../components/PlayerPersonPerson'
 const Fondo_svg = () => {
     return (
         <div class="container">
-        <svg viewBox="0 0 500 500"
-            preserveAspectRatio="xMinYMin meet">      
-            <path d="M0, 150 C150, 250 350,0 500, 150 L500, 00 L0, 0 Z">
-            
-          </path>
-        </svg>
-        <PlayerPersonPerson />
-        <PlayerPersonPc />
-    </div>
+            <svg viewBox="0 0 500 500"
+                preserveAspectRatio="xMinYMin meet">
+                <path d="M0, 150 C150, 250 350,0 500, 150 L500, 00 L0, 0 Z">
+
+                </path>
+            </svg>
+            <PlayerPersonPerson />
+            <PlayerPersonPc />
+        </div>
     )
 }
 export default Fondo_svg;
